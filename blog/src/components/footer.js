c074218930328aff22cd2ce27d129caebf6fc4bd
Conner Chyung <chyungspice@gmail.com>
2020-08-06 22:20:30 -0600

    add footer

---
+++ blog/src/components/footer.js
@@ -0,0 +1,22 @@
+import React from "react"
+import styled from 'styled-components'
+
+let StyledFooter = styled.div`
+    background-color: black;
+    color: white;
+    height: 60px;
+    position: absolute;
+    width: 100%;
+    text-align: center;
+    line-height: 60px;
+    font-family: 'NeurialGrotesk';
+`
+
+const Footer = () => (
+    <StyledFooter>
+        <p>LavaLab 2020. All Rights Reserved.</p>
+    </StyledFooter>
+    
+)
+
+export default Footer
