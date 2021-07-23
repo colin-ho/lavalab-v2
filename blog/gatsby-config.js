fc72ffbd6dae3159f87fffad48c12c13c86603d1
Nikhil Patel <nhpatel@usc.edu>
2021-07-23 08:34:53 -0700

    pushing GTM

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -56,5 +56,30 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-5CMF38L",
+
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        //
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+
+        // Name of the event that is triggered
+        // on every Gatsby route change.
+        //
+        // Defaults to gatsby-route-change
+        routeChangeEventName: "gatsby-route-change",
+        // Defaults to false
+        enableWebVitalsTracking: true,
+      },
+    },
   ],
 }
